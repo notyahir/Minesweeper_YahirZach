4a4c8d08fc3bc9f79450391daacde3c673adfc2a
zach <zacharypmccoy@yahoo.com>
2022-02-28 13:10:39 -0600

    Attempt to revise it without onclick
    trying to get the player to be able to just type in a coord to either place a flag or reveal the spot

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import random
 
-# Defines circle flags for placement
-def flag(x, y):
-    circle = Circle(10)
-    circle.set_color(Color.red)
-    circle.set_position(x, y)
-    add(circle)
-# Creates the mouse event for clicks
-add_mouse_click_handler(flag)
+row = 5
+columns = 5
+#Board idea (make rows/columns)
+row1 = ["1", "2", "3", "4", "5"]
+row2 = ["1", "2", "3", "4", "5"]
+row3 = ["1", "2", "3", "4", "5"]
+row4 = ["1", "2", "3", "4", "5"]
+row5 = ["1", "2", "3", "4", "5"]
 
-# A check on the board places that have been flagged
-flag = []
-# Idk was hoping this might get rid of the flag if one is already in that position
-if [x, y] in flag
-    clear()
+rows = row1, row2, row3, row4, row5
+#I have no idea how to get the player to type in a coord to either place a flag or reveal the spot but
+# that's the goal
+row_coordinate = []
+x = 0
+y = 0
+while x < 5:
+    while y < 5:
+        coordinate.append((x,y))
