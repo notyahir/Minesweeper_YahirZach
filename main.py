fcc185a5ac0079dd0f82e432ce093c408dbdc535
zach <zacharypmccoy@yahoo.com>
2022-04-30 17:01:50 -0500

    Fixed it up for my submission, Jyle take out your name and use diff code when you add mine

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 
-# Note: Found bug that you can use the same coordinate more than one time 6 times in a row to win the game
 # Board
+# Worked with Jyle
 row1 = ["1 ", "2 ", "3 ", "4 ", "5 "]
 row2 = ["6 ", "7 ", "8 ", "9 ", "10"]
 row3 = ["11", "12", "13", "14", "15"]
@@ -17,7 +17,6 @@ board = [
 ]
 
 # Bomb board that is going to be altered to determine where the bombs should be randomized.
-
 rowB1 = ["1 ", "2 ", "3 ", "4 ", "5 "]
 rowB2 = ["6 ", "7 ", "8 ", "9 ", "10"]
 rowB3 = ["11", "12", "13", "14", "15"]
@@ -31,13 +30,10 @@ bomb_Board = [
     rowB4,
     rowB5
 ]
-
-
 # Prints the board after every move
 def print_rows(game_board):
     for i in range(len(game_board)):
         print("  ".join(game_board[i]))
-
 
 def random_bombs():
     for value in range(len(bomb_Board)):
@@ -45,65 +41,61 @@ def random_bombs():
         random_element = random.randint(0, len(bomb_Board) - 1)
         bomb_Board[random_row][random_element] = "Bb"
 
-
 bombHit = False
 value_in_board = False
 
-def flag_place():
-    global bombHit, value_in_board
-    choices = 5
-    while not bombHit:
-        while choices > 0:
-            if bombHit == True:
-                break
-            print()
-            print("You have " + str(choices) + " choices left!")
-            row_coordinate = input("Please choose a spot on the board. ")
-            print()
-            row_coordinate_single = row_coordinate + " "
-            for elements in bomb_Board:
-                if row_coordinate_single in elements:
-                    board[bomb_Board.index(elements)][elements.index(row_coordinate_single)] = "X "
-                    value_in_board = True
-                elif row_coordinate in elements:
-                    board[bomb_Board.index(elements)][elements.index(row_coordinate)] = "X "
-                    value_in_board = True
-                else:
-                    continue
-            if not value_in_board:
-                bombHit = True
-                continue
-            else:
-                value_in_board = False
-                print_rows(board)
-
-            choices -= 1
-        if choices == 0:
-            print("No more choices so you win!")
-            break
-
-
-    if bombHit:
-        print("You hit a bomb, you lose")
-        # print_rows(bomb_Board)
-    else:
-        print("You win!")
-
-
-
 # Defines the coordinates the user can select (1-25), if they hit a bomb ton the board.hey lose (5 bombs), user is
 # prompted to enter a new coordinate if they input one that is not listed
+# Worked with Jyle
+def flag_place(play):
+    global bombHit, value_in_board
+    if play == "Yes":
+        choices = 5
+        while not bombHit:
+            while choices > 0:
+                if bombHit == True:
+                    break
+                print()
+                print("You have " + str(choices) + " choices left!")
+                row_coordinate = input("Please choose a spot on the board. ")
+                print()
+                row_coordinate_single = row_coordinate + " "
+                # Worked with Jyle
+                for elements in bomb_Board:
+                    if row_coordinate_single in elements:
+                        board[bomb_Board.index(elements)][elements.index(row_coordinate_single)] = "X "
+                        value_in_board = True
+                    elif row_coordinate in elements:
+                        board[bomb_Board.index(elements)][elements.index(row_coordinate)] = "X "
+                        value_in_board = True
+                    else:
+                        continue
+                if not value_in_board:
+                    bombHit = True
+                    continue
+                else:
+                    value_in_board = False
+                    print_rows(board)
 
+                choices -= 1
+            # Defines a win function if the user successfully avoids all bombs by the end of their moves
+            if choices == 0:
+                print("No more choices so you win!")
+                break
 
-# Defines a win function if the user successfully avoids all bombs by the end of their moves
-def win():
-    if row1[1] and row2[1] and row3[0] and row3[3] and row4[0] != "X":
-        print("You won!")
+        if bombHit:
+            print("You hit a bomb, you lose")
+            # print_rows(bomb_Board)
+        else:
+            print("You win!")
+    elif play == "No":
+        print("Rerun Program to try again")
 
-
+# Worked with Jyle
 if __name__ == "__main__":
     print_rows(board)
     print()
     random_bombs()
+    play_game = str(input("Do you want to play the game? Yes or No: "))
     # Calls place_flag function
-    flag_place()
+    flag_place(play_game)
